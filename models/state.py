2c1d4aa9e2b9c1be5b1a489355db5668bcf318f3
octave collombel <octavec@outlook.com>
2021-08-09 20:59:40 +0200

    temporary

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from models import storage
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = "states"
+    @property
+    def cities(self):
+    	list = []
+    	print(storage.all(City))
+    	print("HERE")
