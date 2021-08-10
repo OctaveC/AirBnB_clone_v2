979913e7355514a29aeb6ee51535bdaec37fc2b6
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-10 22:04:19 +0200

    Add test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,7 +3,6 @@
 import os
 import unittest
 import models
-import pep8
 from console import HBNBCommand
 from models.base_model import BaseModel
 from models.user import User
@@ -36,16 +35,4 @@ class TestHBNBCommand(unittest.TestCase):
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
 
-    def test_pep8_conformance_console(self):
-        """Test console.py is PEP8 """
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
 
-    def test_pep8_conformance_test_console(self):
-        """Test test_console.py is PEP8 """
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
