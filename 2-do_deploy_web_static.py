eee18dd11366c652529173ab26f70bb3d67f5274
octave collombel <octavec@outlook.com>
2021-08-26 17:51:28 +0200

    exo 3 testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local, put, run, env
 from datetime import datetime
 import os.path
 
-env.hosts = ['34.73.164.98', '34.139.45.21']
+env.hosts = ["34.73.164.98", "34.139.45.21"]
 
 
 def do_deploy(archive_path):
@@ -16,20 +16,20 @@ def do_deploy(archive_path):
     """
     if os.path.exists(archive_path) is False:
         return False
-    archive_split_slash = archive_path.split('/')[1]
+    archive_split_slash = archive_path.split("/")[1]
     archive_split_dot = archive_split_slash.split(".")[0]
 
     path_dot = "/data/web_static/releases/" + archive_split_dot
-    path_slash = '/tmp/' + archive_split_slash
+    path_slash = "/tmp/" + archive_split_slash
     put(archive_path, path_slash)
 
-    run('mkdir -p ' + path_dot)
-    run('tar -xzf /tmp/{} -C {}/'.format(archive_split_slash, path_dot))
-    run('rm {}'.format(path_slash))
+    run("mkdir -p " + path_dot)
+    run("tar -xzf /tmp/{} -C {}/".format(archive_split_slash, path_dot))
+    run("rm {}".format(path_slash))
 
-    run('mv ' + path_dot + '/web_static/* ' + path_dot + '/')
+    run("mv " + path_dot + "/web_static/* " + path_dot + "/")
 
-    run('rm -rf ' + path_dot + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + path_dot + ' /data/web_static/current')
+    run("rm -rf " + path_dot + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + path_dot + " /data/web_static/current")
     return True
