767090befe3a8326029f05855e4b4de859258629
octave collombel <octavec@outlook.com>
2021-08-26 16:08:58 +0200

    exo 2 fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 """
-Generates a .tgz archive from the contents of the web_static
-folder of our AirBnB Clone repo, using the function do_pack.
+Distributes an archive to your web servers,
+using the function do_deploy.
 """
-from fabric.api import local
+from fabric.api import local, put, run, env
 from datetime import datetime
 import os.path
 
+env.hosts = ["34.73.164.98", "34.139.45.21"]
 
-def do_pack():
+
+def do_deploy(archive_path):
     """
-    Generate a .tgz archive.
+    Distributes an archive to your web servers.
     """
-    local("mkdir -p versions")
-    datet = datetime.now().strftime("%Y%m%d%H%M%S")
-    location = "versions/web_static_{}.tgz".format(datet)
-    local("tar -cvzf " + location + " web_static")
-    if os.path.exists(location):
-        return location
-    return None
+    if os.path.exists(archive_path) is False:
+        return False
+    archive_split_slash = archive_path.split("/")[1]
+    archive_split_dot = archive_split_slash.split(".")[0]
+
+    path_no_dot = "/data/web_static/releases/" + archive_split_dot
+    path_no_slash = "/tmp/" + archive_split_slash
+    put(archive_path, path_no_slash)
+
+    run("mkdir -p " + path_no_dot)
+    run("tar -xzf /tmp/{} -C {}/".format(archive_split_slash, path_no_dot))
+    run("rm {}".format(path_no_slash))
+
+    run("mv " + path_no_dot + "/web_static/* " + path_no_dot + "/")
+    run("rm -rf " + path_no_dot + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + path_no_dot + " /data/web_static/current")
+    return True
