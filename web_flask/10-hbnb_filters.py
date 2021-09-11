994cfc99b93aae65beda3ed1f27549cadabe2a47
octave collombel <octavec@outlook.com>
2021-09-11 16:32:14 +0200

    exo 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Launches Flask Web App
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters(id=None):
+    """ Displays an HTML page for filters """
+    stuff = storage.all(State)
+    states = []
+
+    for ite in stuff:
+        states.append(stuff[ite])
+
+    stuff = storage.all(Amenity)
+    amenities = []
+
+    for ite in stuff:
+        amenities.append(stuff[ite])
+
+    return render_template('10-hbnb_filters.html', states=states,
+        amenities=amenities)
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """ Closes SQALchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
